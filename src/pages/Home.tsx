559bbf1313d47a17ba23f4f71a00ba62716cbdbb
rjfm <javier.fuentesm@hotmail.com>
2019-11-15 02:13:31 -0600

    Promer ejemplo en we y tambien en movil

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,61 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import React from "react";
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonContent,
+  IonIcon,
+  IonItem,
+  IonLabel
+} from "@ionic/react";
 
-const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Ionic Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
-      </IonContent>
-    </IonPage>
-  );
-};
+export const Home: React.FC = () => (
+  <IonContent>
+    <IonCard>
+      <IonCardHeader>
+        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+        <IonCardTitle>Card Title</IonCardTitle>
+      </IonCardHeader>
+
+      <IonCardContent>
+        Keep close to Nature's heart... and break clear away, once in awhile,
+        and climb a mountain or spend a week in the woods. Wash your spirit
+        clean.
+      </IonCardContent>
+    </IonCard>
+
+    <IonCard>
+      <IonItem>
+        <IonIcon name="pin" slot="start" />
+        <IonLabel>ion-item in a card, icon left, button right</IonLabel>
+        <IonButton fill="outline" slot="end">
+          View
+        </IonButton>
+      </IonItem>
+
+      <IonCardContent>
+        This is content, without any paragraph or header tags, within an
+        ion-cardContent element.
+      </IonCardContent>
+    </IonCard>
+
+    <IonCard>
+      <IonItem href="#" class="activated">
+        <IonIcon name="wifi" slot="start" />
+        <IonLabel>Card Link Item 1 .activated</IonLabel>
+      </IonItem>
+
+      <IonItem href="#">
+        <IonIcon name="wine" slot="start" />
+        <IonLabel>Card Link Item 2</IonLabel>
+      </IonItem>
+
+
+    </IonCard>
+  </IonContent>
+);
 
 export default Home;
